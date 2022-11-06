a99587ae76f2769ae8e853f2667597c5090b3c48
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-11-06 22:42:03 +0500

    init:copy function added

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcCopy = path.join(__dirname, 'files-copy');
+const srcOrigin = path.join(__dirname, 'files');
+fs.mkdir(srcCopy, {recursive: true}, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+});
+
+fs.readdir(srcCopy, async (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    await files.forEach(file => {
+      fs.unlink(path.join(srcCopy, file), (err) => {
+          if (err) console.log(err);
+        })
+    })
+
+    fs.readdir(srcOrigin, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fs.copyFile(path.join(srcOrigin, file), path.join(srcCopy, file), (err) => {
+              if (err) console.log(err);
+              console.log(`file ${file} was copied`);
+            })
+        })
+      }
+  });
+  }
+});
